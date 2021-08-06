4f650b1f46b2aa8da68786b13c89b1cf7968ab33
Wenbo-Ge <wge19920921@gmail.com>
2021-08-06 16:06:37 -0400

    more contents

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -11,16 +11,31 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import speakCN from "../assets/audio/CNword.mp3";
-import quiz1_0 from "../assets/images/olympic_simply_chinese.png"
-import quiz1_1 from "../assets/images/olympic_traditional_chinese.png"
+import news_cn from "../assets/audio/news_cn.mp3";
+import news_kor from "../assets/audio/news_kor.mp3";
+import news_jap from "../assets/audio/news_jap.mp3";
+import nation_cn from "../assets/audio/nation_cn.mp3";
+import nation_kor from "../assets/audio/nation_kor.mp3";
+import nation_jap from "../assets/audio/nation_jap.mp3";
+import quiz1_0 from "../assets/images/olympic_simply_chinese.png";
+import quiz1_1 from "../assets/images/olympic_traditional_chinese.png";
+import quiz2_0 from "../assets/images/usa_cn.png";
+import quiz2_1 from "../assets/images/usa_kor.png";
+import quiz2_2 from "../assets/images/usa_jap.png";
+import quiz3_0 from "../assets/images/canada_jap.png";
+import quiz3_1 from "../assets/images/canada_cn.png";
+import quiz3_2 from "../assets/images/canada_kor.png";
+import quiz4_0 from "../assets/images/eco_jap.png";
+import quiz4_1 from "../assets/images/eco_tra_cn.png";
+import quiz4_2 from "../assets/images/eco_cn.png";
 
 export default class Game extends Component {
     constructor(props) {
         super(props);
         this.state = {
           pageCorrectness: ["default","default","default","default","default","default"],
-          speakCN: new Audio(speakCN)
+          listening_1: [new Audio(news_cn), new Audio(news_kor), new Audio(news_jap)],
+          listening_2: [new Audio(nation_cn), new Audio(nation_kor), new Audio(nation_jap)],
         };
       }
   
@@ -35,9 +50,53 @@ export default class Game extends Component {
             });
     }
 
-    playOne = () => {
-          this.state.speakCN.play();
+    showAnswerThree () {
+      this.setState({
+        pageCorrectness: ["default","default","correct","default","default","default"]
+    });
+    }
+
+    showAnswerFour () {
+      this.setState({
+        pageCorrectness: ["default","default","default","correct","default","default"]
+    });
+    }
+
+    showAnswerFive () {
+      this.setState({
+        pageCorrectness: ["default","default","default","default","correct","default"]
+    });
+    }
+
+    showAnswerSix () {
+      this.setState({
+        pageCorrectness: ["default","default","default","default","default","correct"]
+    });
+    }
+
+    listening_1_cn = () => {
+          this.state.listening_1[0].play();
       };
+
+    listening_1_kor = () => {
+        this.state.listening_1[1].play();
+    };
+
+    listening_1_jap = () => {
+      this.state.listening_1[2].play();
+  };
+
+  listening_2_cn = () => {
+    this.state.listening_2[0].play();
+};
+
+listening_2_kor = () => {
+  this.state.listening_2[1].play();
+};
+
+listening_2_jap = () => {
+this.state.listening_2[2].play();
+};
 
 
   render() {
@@ -49,6 +108,10 @@ export default class Game extends Component {
       slidesToScroll: 1,
       adaptiveHeight: true
     };
+    const styleObj = {
+      columns: 3,
+      padding: '4%',
+    };
     return (
       <div className="Game">
         <h1 style={{marginBottom:"5%"}}>East Asia Languages Quiz</h1>
@@ -56,7 +119,7 @@ export default class Game extends Component {
           <div>
           <Card style={{backgroundColor: "gainsboro"}}>
           <Typography gutterBottom variant="h4" component="h4">
-              Quiz 1: English Word "Olympic Games" in Chinese
+              Quiz 1-2: English Word "Olympic Games" in Chinese
           </Typography>   
                 <CardMedia
                 style={{height: "100px",
@@ -97,32 +160,51 @@ export default class Game extends Component {
           <div>
           <Card style={{backgroundColor: "gainsboro"}}>
           <Typography gutterBottom variant="h4" component="h4">
-              Quiz 1: English Word "Olympic Games" in Chinese
+              Quiz 3-5: English Word "United State of America" in Chinese/Japanese/Korean
           </Typography>   
                 <CardMedia
                 style={{height: "100px",
                         backgroundSize: "contain"}}
-                image={quiz1_0}
+                image={quiz2_0}
                 title="Quiz 1"
                 />
                 <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">
-                    <span><b>A. This is Traditional Chinese</b></span>
+                    <span className={this.state.pageCorrectness[1]}><b>A. This is Chinese</b></span>
                     <br />
-                    <span className={this.state.pageCorrectness[1]}><b>B. This is Simplified Chinese</b></span> 
+                    <span><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span><b>C. This is Korean</b></span> 
                 </Typography>
                 </CardContent>
                 <CardMedia
                 style={{height: "100px",
                         backgroundSize: "contain"}}
-                image={quiz1_1}
+                image={quiz2_1}
                 title="Quiz 1"
                 />
                 <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">
-                    <span className={this.state.pageCorrectness[1]}><b>A. This is Traditional Chinese</b></span>
+                    <span><b>A. This is Chinese</b></span>
                     <br />
-                    <span><b>B. This is Simplified Chinese</b></span> 
+                    <span><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span className={this.state.pageCorrectness[1]}><b>C. This is Korean</b></span> 
+                </Typography>
+                </CardContent>
+                <CardMedia
+                style={{height: "100px",
+                        backgroundSize: "contain"}}
+                image={quiz2_2}
+                title="Quiz 1"
+                />
+                <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <span><b>A. This is Chinese</b></span>
+                    <br />
+                    <span className={this.state.pageCorrectness[1]}><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span><b>C. This is Korean</b></span> 
                 </Typography>
                 </CardContent>
             <CardActions style={{display:"inline-flex"}}>
@@ -136,18 +218,236 @@ export default class Game extends Component {
           </Card>
           </div>
           <div>
-             <IconButton aria-label="play/pause" onClick={this.playOne}>
-                <PlayArrowIcon />
-             </IconButton>
+          <Card style={{backgroundColor: "gainsboro"}}>
+          <Typography gutterBottom variant="h4" component="h4">
+              Quiz 6-8: English Word "Canada" in Chinese/Japanese/Korean
+          </Typography>   
+                <CardMedia
+                style={{height: "100px",
+                        backgroundSize: "contain"}}
+                image={quiz3_0}
+                title="Quiz 1"
+                />
+                <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <span><b>A. This is Chinese</b></span>
+                    <br />
+                    <span className={this.state.pageCorrectness[2]}><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span><b>C. This is Korean</b></span> 
+                </Typography>
+                </CardContent>
+                <CardMedia
+                style={{height: "100px",
+                        backgroundSize: "contain"}}
+                image={quiz3_1}
+                title="Quiz 1"
+                />
+                <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <span className={this.state.pageCorrectness[2]}><b>A. This is Chinese</b></span>
+                    <br />
+                    <span><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span><b>C. This is Korean</b></span> 
+                </Typography>
+                </CardContent>
+                <CardMedia
+                style={{height: "100px",
+                        backgroundSize: "contain"}}
+                image={quiz3_2}
+                title="Quiz 1"
+                />
+                <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <span><b>A. This is Chinese</b></span>
+                    <br />
+                    <span><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span className={this.state.pageCorrectness[2]}><b>C. This is Korean</b></span> 
+                </Typography>
+                </CardContent>
+            <CardActions style={{display:"inline-flex"}}>
+                <span>Show answers: </span>
+                <IconButton color="secondary" aria-label="add an alarm" 
+                onClick={() => this.showAnswerThree()}
+                >
+                <VisibilityIcon/>
+              </IconButton>
+            </CardActions>
+          </Card>
           </div>
           <div>
-            <h3>4</h3>
+          <Card style={{backgroundColor: "gainsboro"}}>
+          <Typography gutterBottom variant="h4" component="h4">
+              Quiz 9-11: English Word "Economy" in Simlified/Traditional Chinese/Japanese
+          </Typography>   
+                <CardMedia
+                style={{height: "100px",
+                        backgroundSize: "contain"}}
+                image={quiz4_0}
+                title="Quiz 1"
+                />
+                <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <span><b>A. This is Simplified Chinese</b></span>
+                    <br />
+                    <span className={this.state.pageCorrectness[3]}><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span><b>C. This is Traditional Chinese</b></span> 
+                </Typography>
+                </CardContent>
+                <CardMedia
+                style={{height: "100px",
+                        backgroundSize: "contain"}}
+                image={quiz4_1}
+                title="Quiz 1"
+                />
+                <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <span><b>A. This is Simplified Chinese</b></span>
+                    <br />
+                    <span><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span className={this.state.pageCorrectness[3]}><b>C. This is Traditional Chinese</b></span> 
+                </Typography>
+                </CardContent>
+                <CardMedia
+                style={{height: "100px",
+                        backgroundSize: "contain"}}
+                image={quiz4_2}
+                title="Quiz 1"
+                />
+                <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <span className={this.state.pageCorrectness[3]}><b>A. This is Simplified Chinese</b></span>
+                    <br />
+                    <span><b>B. This is Japanese</b></span> 
+                    <br />
+                    <span><b>C. This is Traditional Chinese</b></span> 
+                </Typography>
+                </CardContent>
+            <CardActions style={{display:"inline-flex"}}>
+                <span>Show answers: </span>
+                <IconButton color="secondary" aria-label="add an alarm" 
+                onClick={() => this.showAnswerFour()}
+                >
+                <VisibilityIcon/>
+              </IconButton>
+            </CardActions>
+          </Card>
           </div>
+          
           <div>
-            <h3>5</h3>
+          <Card style={{backgroundColor: "gainsboro"}}>
+          <Typography gutterBottom variant="h4" component="h4">
+              Quiz 12-14: Pronunciations of "I am Chinese/Japanese/Korean" in Chinese/Japanese/Korean
+          </Typography>
+          <CardContent>
+            <div className="question_sec" style={styleObj}>
+              <div className="question">
+                <IconButton aria-label="play/pause" onClick={this.listening_2_kor}>
+                  <PlayArrowIcon color="secondary" aria-label="add an alarm"/>
+              </IconButton>
+              <Typography variant="body2" color="textSecondary" component="p">
+                      <span><b>A. This is Chinese</b></span>
+                      <br />
+                      <span><b>B. This is Japanese</b></span> 
+                      <br />
+                      <span className={this.state.pageCorrectness[4]}><b>C. This is Korean</b></span> 
+                </Typography>
+              </div>
+              <div className="question">
+                <IconButton aria-label="play/pause" onClick={this.listening_2_jap}>
+                  <PlayArrowIcon color="secondary" aria-label="add an alarm"/>
+              </IconButton>
+              <Typography variant="body2" color="textSecondary" component="p">
+                      <span><b>A. This is Chinese</b></span>
+                      <br />
+                      <span  className={this.state.pageCorrectness[4]}><b>B. This is Japanese</b></span> 
+                      <br />
+                      <span><b>C. This is Korean</b></span> 
+                </Typography>
+              </div>
+              <div className="question">
+                <IconButton aria-label="play/pause" onClick={this.listening_2_cn}>
+                  <PlayArrowIcon color="secondary" aria-label="add an alarm"/>
+              </IconButton>
+              <Typography variant="body2" color="textSecondary" component="p">
+                      <span className={this.state.pageCorrectness[4]}><b>A. This is Chinese</b></span>
+                      <br />
+                      <span><b>B. This is Japanese</b></span> 
+                      <br />
+                      <span><b>C. This is Korean</b></span> 
+                </Typography>
+              </div>
+            </div> 
+            </CardContent>
+            <CardActions style={{display:"inline-flex"}}>
+                <span>Show answers: </span>
+                <IconButton color="secondary" aria-label="add an alarm" 
+                onClick={() => this.showAnswerFive()}
+                >
+                <VisibilityIcon/>
+              </IconButton>
+            </CardActions>
+          </Card>
           </div>
+
           <div>
-            <h3>6</h3>
+          <Card style={{backgroundColor: "gainsboro"}}>
+          <Typography gutterBottom variant="h4" component="h4">
+              Quiz 15-17: News recordings in Chinese/Japanese/Korean
+          </Typography>
+          <CardContent>
+            <div className="question_sec" style={styleObj}>
+              <div className="question">
+                <IconButton aria-label="play/pause" onClick={this.listening_1_cn}>
+                  <PlayArrowIcon color="secondary" aria-label="add an alarm"/>
+              </IconButton>
+              <Typography variant="body2" color="textSecondary" component="p">
+                      <span  className={this.state.pageCorrectness[5]}><b>A. This is Chinese</b></span>
+                      <br />
+                      <span><b>B. This is Japanese</b></span> 
+                      <br />
+                      <span><b>C. This is Korean</b></span> 
+                </Typography>
+              </div>
+              <div className="question">
+                <IconButton aria-label="play/pause" onClick={this.listening_1_jap}>
+                  <PlayArrowIcon color="secondary" aria-label="add an alarm"/>
+              </IconButton>
+              <Typography variant="body2" color="textSecondary" component="p">
+                      <span><b>A. This is Chinese</b></span>
+                      <br />
+                      <span  className={this.state.pageCorrectness[5]}><b>B. This is Japanese</b></span> 
+                      <br />
+                      <span><b>C. This is Korean</b></span> 
+                </Typography>
+              </div>
+              <div className="question">
+                <IconButton aria-label="play/pause" onClick={this.listening_1_kor}>
+                  <PlayArrowIcon color="secondary" aria-label="add an alarm"/>
+              </IconButton>
+              <Typography variant="body2" color="textSecondary" component="p">
+                      <span><b>A. This is Chinese</b></span>
+                      <br />
+                      <span><b>B. This is Japanese</b></span> 
+                      <br />
+                      <span className={this.state.pageCorrectness[5]}><b>C. This is Korean</b></span> 
+                </Typography>
+              </div>
+            </div> 
+            </CardContent>
+            <CardActions style={{display:"inline-flex"}}>
+                <span>Show answers: </span>
+                <IconButton color="secondary" aria-label="add an alarm" 
+                onClick={() => this.showAnswerSix()}
+                >
+                <VisibilityIcon/>
+              </IconButton>
+            </CardActions>
+          </Card>
           </div>
         </Slider>
       </div>
--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,6 +1,8 @@
 import React from 'react';
  
 import { NavLink } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+import GTranslateIcon from '@material-ui/icons/GTranslate';
  
 const Navigation = () => {
     const styleObj = {
@@ -9,10 +11,10 @@ const Navigation = () => {
     return (
        <div className="Nav" style={styleObj}>
            <div className="Nav1" style={{textAlign:"left"}}>
-           <NavLink to="/react-project">VirtualMicrobit</NavLink>
+           <NavLink to="/react-project"><HomeIcon color="secondary" /></NavLink>
            </div>
            <div className="Nav2" style={{textAlign:"right"}}>
-           <NavLink to="/game">Game</NavLink>
+           <NavLink to="/game"><GTranslateIcon color="secondary" /></NavLink>
            </div>
           
           
